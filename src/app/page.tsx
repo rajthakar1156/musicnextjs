95fc26093aac2d34da3c57864011ed92266ca4ba
rajthakar1156 <rajthakar1156@gmail.com>
2025-07-11 18:20:42 +0530

    Add HeroSection component and Spotlight animation and button animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
+import HeroSection from "@/components/HeroSection";
 
 import { div, main } from "motion/react-client";
 
 export default function Home() {
   return (
     <main>
-       <h1 className="text-2xl text-center">chai code</h1>
+       <HeroSection/>
     </main>
   );
 }
