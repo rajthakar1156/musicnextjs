0a24aee5c5410a523e6ecd269115a9bb563b7b69
rajthakar1156 <rajthakar1156@gmail.com>
2025-07-12 11:38:50 +0530

    Add FeaturedCourses component and BackgroundGradient for course display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 import { div, main } from "motion/react-client";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main>
        <HeroSection/>
+       <FeaturedCourses/>
     </main>
   );
 }
